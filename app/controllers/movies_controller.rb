a843b12826749a8c2b25e651accff89e2a6dd916
EC2 Default User <ec2-user@ip-172-31-25-147.us-east-2.compute.internal>
2019-10-03 22:52:18 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,6 +12,25 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all.order(params[:sort])
+    
+    # reference: https://stackoverflow.com/questions/9658881/rails-select-unique-values-from-a-column
+    
+    @all_ratings = Movie.order(:rating).select(:rating).map(&:rating).uniq
+    
+    @selected = @all_ratings
+   
+    if params[:ratings]
+      @selected = params[:ratings].keys
+    end
+
+    @selected.each do |rating|
+      params[rating] = 1
+    end
+
+    if params[:ratings]
+      @movies = Movie.where(rating: @selected)
+    end
+    
   end
 
   def new
