108b9602cc6fa72d984ee5019f036b1fa48da36c
EC2 Default User <ec2-user@ip-172-31-25-147.us-east-2.compute.internal>
2019-10-03 02:50:25 +0000

    test heroku

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    # @movies = Movie.all
+    @movies = Movie.all.order(params[:sort])
   end
 
   def new
